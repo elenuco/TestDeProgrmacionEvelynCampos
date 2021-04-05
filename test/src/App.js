7e975a28d961e4139bcaf50f0d4ee48927c7c1c9
elenuco <elenuco36@gmail.com>
2021-04-05 01:24:45 +0000

    cambios para los todos

--- test/src/App.js
+++ test/src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const url = ' https://0q27loouph.execute-api.us-east-1.amazonaws.com/ '
+    const [todos, setTodos] = useState()
+    const fetchApi= async()=>{
+        const response= await fetch(url)
+        const responseJson= await response.json()
+        setTodos(responseJson)
+    }
+    useEffect(() => {
+    fetchApi()
+    },[])
   return (
     <div className="App">
       hola mundo
